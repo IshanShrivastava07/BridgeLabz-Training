9e61b5a52ee3af1fd76f177dc1c8b3879500e096
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-18 15:08:57 +0530

    [Ishan Shrivastava] : added AgeOfHarry.java

---
+++ gcr-codebase/programming-elements/level-one/AgeOfHarry.java
@@ -0,0 +1,12 @@
+//creating class AgeOfHarry for finding out Harry's Age
+class AgeOfHarry{
+	public static void main(String args[])
+	{
+		//Current Year is 2024 
+		int currYear  = 2024;
+		// Harry's Birth Year
+		int birthYear = 2000;
+		//Substracting current Year and Harry's Birth Year to get Haryy's Age
+		System.out.print(currYear - birthYear);
+	}
+}
