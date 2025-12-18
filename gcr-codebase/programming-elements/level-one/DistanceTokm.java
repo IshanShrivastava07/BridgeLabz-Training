aa35e34a0a65b9282eb901a24e243b357b1d4a2d
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-18 15:28:34 +0530

    [Ishan Shrivastava] added DistanceToKm

---
+++ gcr-codebase/programming-elements/level-one/DistanceTokm.java
@@ -0,0 +1,11 @@
+//creating class to convert km to miles
+class DistanceToKm{
+	public static void main(String args[]){
+		//distance in km
+		double km = 10.8;
+		//distance in miles
+		double distanceInMiles = 10.8 * 1.6;
+		//displaying output
+		System.out.println("The distance 10.8km in miles is: " + distanceInMiles);
+	}
+}
