cd4e49a3f8c8564b188c6eea44d7f01d19ef8de9
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-18 15:10:03 +0530

    [Ishan Shrivastava] : added SamAverageMarks.java

---
+++ gcr-codebase/programming-elements/level-one/SamAverageMarks.java
@@ -0,0 +1,16 @@
+//creating class SamAverageMarks to calculate Sam's average marks in Physics, Maths and Chemistry
+class SamAverageMarks{
+	public static void main(String args[]){
+		//Sam's Physics's Marks
+		int PhysicsMarks = 95;
+		//Sam's Maths Marks 
+		int MathsMarks = 94;
+		//Sam's Chemistry Marks 
+		int ChemistryMarks = 96;
+		//Sam's Average Score
+		int average = (PhysicsMarks + MathsMarks + ChemistryMarks)/3;
+		//Displaying Output
+		System.out.print(average);
+	}
+}
+		
