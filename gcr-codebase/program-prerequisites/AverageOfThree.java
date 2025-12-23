d465b261355aa21a320bb93fec97a0ae2dfeee7b
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-23 11:27:46 +0530

    [Ishan Shrivastava] : updated AverageOfThree

--- gcr-codebase/program-prerequisites/AverageOfThree.java
+++ gcr-codebase/program-prerequisites/AverageOfThree.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class AverageOfThree {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
