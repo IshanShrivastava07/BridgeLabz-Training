45fe9fcfa28ddb2cb912dbb1ebc2b20ebe860403
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-26 00:28:31 +0530

    [Ishan Shrivastava] : added MaximumOfThree

---
+++ gcr-codebase/extras/extra-Built-in/Additional/MaximumOfThree.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// Creating class  MaximumOfThree to find max average of three numbers
+public class MaximumOfThree {
+
+    static int findMaximum(int a, int b, int c) {
+       int max = a;
+       if (b > max) max = b;
+       if (c > max) max = c;
+       return max;
+    }
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       System.out.print("Enter first number: ");
+       int a = sc.nextInt();
+
+       System.out.print("Enter second number: ");
+       int b = sc.nextInt();
+
+       System.out.print("Enter third number: ");
+       int c = sc.nextInt();
+
+       System.out.println("Maximum number is: " + findMaximum(a, b, c));
+    }
+}
