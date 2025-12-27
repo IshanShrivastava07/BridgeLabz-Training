24da347192a9360b1fa87cf0fb78dc3aaec26bca
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-27 16:06:39 +0530

    [Ishan Shrivastava] : added Employee

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/levelone/Employee.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/levelone/Employee.java
@@ -1,10 +1,27 @@
 package com.bridgelabz.oops.levelone;
 
-public class Employee {
+class Employee {
+   // Instance variables
+    String name;
+    int id;
+    double salary;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // Constructor
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
 
-	}
+    // Method to display employee details
+    void displayEmployee() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee id: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
 
+    public static void main(String[] args) {
+        Employee emp = new Employee("Rohan", 1, 500000);
+        emp.displayEmployee();
+    }
 }
