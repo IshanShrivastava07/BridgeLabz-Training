6d0d38b349732abdb86e2e7859f504e54e879ea5
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-27 16:11:56 +0530

    [Ishan Shrivastava] : StudentsGrades

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/leveltwo/StudentsGrades.java
@@ -0,0 +1,59 @@
+package com.bridgelabz.oops.leveltwo;
+
+
+public class StudentsGrades {
+
+    // Student attributes
+    String name;
+    String rollNumber;
+    double[] marks;
+
+    // Constructor
+    StudentsGrades(String name, String rollNumber, double[] marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+    // Method to calculate grade
+    public String calculateGrade() {
+        double average = calculateAverageMarks();
+
+        if (average >= 80) return "Grade A";
+        else if (average >= 60) return "Grade B";
+        else if (average >= 50) return "Grade C";
+        else if (average >= 40) return "Grade D";
+        else return "Grade F";
+    }
+
+    // Method to calculate average marks
+    public double calculateAverageMarks() {
+        double sum = 0;
+        for (double mark : marks) {
+            sum += mark;
+        }
+        return sum / marks.length;
+    }
+
+    // Method to display student details
+    public void displayStudent() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student RollNumber: " + rollNumber);
+        System.out.println("Student Marks:");
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("Mark " + (i + 1) + ": " + marks[i]);
+        }
+        System.out.println(calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        double[] mark1 = {80, 70, 75};
+        double[] mark2 = {60, 65, 50};
+
+        StudentsGrades student1 = new StudentsGrades("Thamarai", "ECE001", mark1);
+        StudentsGrades student2 = new StudentsGrades("Kannan", "CSC002", mark2);
+
+        student1.displayStudent();
+        student2.displayStudent();
+    }
+}
