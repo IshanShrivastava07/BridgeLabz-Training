993d927afd1b98224d6fe2fdd558c3388d7428c0
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-29 17:16:28 +0530

    [Ishan Shrivastava] : added Manager

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/levelone/Manager.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.constructor.levelone;
+
+public class Manager extends Employee {
+
+    public Manager(int employeeID, String department, double salary) {
+        super(employeeID, department, salary);
+    }
+}
