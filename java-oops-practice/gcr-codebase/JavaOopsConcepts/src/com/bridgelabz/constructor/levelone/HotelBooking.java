0680d8cd5876c450bbd5ad3e98d747694813887e
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-29 11:50:05 +0530

    [Ishan Shrivastava] : added HotelBooking

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/levelone/HotelBooking.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.constructor.levelone;
+// Hotel Booking System
+public class HotelBooking {
+    String guestName, roomType;
+    int nights;
+
+    HotelBooking() {
+        guestName = "Guest";
+        roomType = "Standard";
+        nights = 1;
+    }
+
+    HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    HotelBooking(HotelBooking hb) {
+        this.guestName = hb.guestName;
+        this.roomType = hb.roomType;
+        this.nights = hb.nights;
+    }
+
+    public static void main(String[] args) {
+        HotelBooking h1 = new HotelBooking("Rahul", "Deluxe", 3);
+        HotelBooking h2 = new HotelBooking(h1);
+        System.out.println(h2.guestName + " " + h2.roomType);
+    }
+}
