5dc06fd1f5606e9fa37096ccff6efda1c701eb63
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-29 17:16:36 +0530

    [Ishan Shrivastava] : added EBook

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/levelone/EBook.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.constructor.levelone;
+
+public class EBook extends LibBook {
+
+    public EBook(String ISBN, String title, String author) {
+        super(ISBN, title, author);
+    }
+}
