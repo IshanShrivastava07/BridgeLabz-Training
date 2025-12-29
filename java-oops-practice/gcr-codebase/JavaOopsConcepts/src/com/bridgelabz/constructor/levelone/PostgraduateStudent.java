613a734b07c15cb1d765e2b04ca819f93534bbdd
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-29 17:16:41 +0530

    [Ishan Shrivastava] : added PostgraduateStudent

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/levelone/PostgraduateStudent.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.constructor.levelone;
+
+public class PostgraduateStudent extends Student {
+
+    public PostgraduateStudent(int rollNumber, String name, double cgpa) {
+        super(rollNumber, name, cgpa);
+    }
+}
