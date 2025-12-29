1b3ac78478d1944d7595092849f992107838b967
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-29 17:16:33 +0530

    [Ishan Shrivastava] : added SavingsAccount

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/constructor/levelone/SavingsAccount.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.constructor.levelone;
+
+public class SavingsAccount extends BankAccount {
+
+    public SavingsAccount(int accountNumber, String accountHolder, double balance) {
+        super(accountNumber, accountHolder, balance);
+    }
+}
