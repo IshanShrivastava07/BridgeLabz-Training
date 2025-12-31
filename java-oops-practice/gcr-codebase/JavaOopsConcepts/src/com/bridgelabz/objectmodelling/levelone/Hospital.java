31418c88e5630ce4b4e5711e155f8214169b41b3
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-31 14:51:46 +0530

    [Ishan Shrivastava] : added Hospital

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/Hospital.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+import java.util.ArrayList;
+
+//Hospital associates Doctors and Patients
+public class Hospital {
+ ArrayList<Doctor> doctors = new ArrayList<>();
+ ArrayList<Patient> patients = new ArrayList<>();
+}
