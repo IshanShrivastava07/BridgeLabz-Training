1ced46b9104c0f6daa883db687b8c17996e03b40
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-31 14:52:02 +0530

    [Ishan Shrivastava] : added School

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/School.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+import java.util.ArrayList;
+
+//School aggregates students
+public class School {
+ ArrayList<Student> students = new ArrayList<>();
+}
+
