c912ed2971e89b46484e21bf95a44c1118def1b7
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-31 14:51:44 +0530

    [Ishan Shrivastava] : added FacultyDepartment

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/FacultyDepartment.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+//Faculty can exist without University
+//Department cannot exist without University
+public class FacultyDepartment {
+ String departmentName;
+
+ public FacultyDepartment(String departmentName) {
+     this.departmentName = departmentName;
+ }
+}
+
