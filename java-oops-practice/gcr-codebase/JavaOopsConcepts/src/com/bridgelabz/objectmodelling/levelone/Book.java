8704153afaa216df78627ec5591f369b12d5fe6e
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-31 14:50:48 +0530

    [Ishan Shrivastava] : added Book

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/Book.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+// Book exists independently
+public class Book {
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
