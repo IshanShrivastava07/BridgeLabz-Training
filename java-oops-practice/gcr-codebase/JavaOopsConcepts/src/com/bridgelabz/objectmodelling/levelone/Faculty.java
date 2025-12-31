0633345a34888b7873da3dd4dde542d12c38cecc
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-31 14:51:41 +0530

    [Ishan Shrivastava] : added Faculty

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/Faculty.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+//Faculty can exist without University
+public class Faculty {
+ String name;
+
+ public Faculty(String name) {
+     this.name = name;
+ }
+}
