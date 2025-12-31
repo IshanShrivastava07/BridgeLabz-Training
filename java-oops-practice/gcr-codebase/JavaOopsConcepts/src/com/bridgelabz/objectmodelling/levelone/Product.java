7106462db1cc988c1b5571def3782ec9e03a6824
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2025-12-31 14:51:58 +0530

    [Ishan Shrivastava] : added Product

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/objectmodelling/levelone/Product.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.objectmodelling.levelone;
+
+public class Product {
+    String productName;
+
+    public Product(String productName) {
+        this.productName = productName;
+    }
+}
+
