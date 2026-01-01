48f0e6944c582484bcd188d5314cb004acdbae2c
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-01 13:28:00 +0530

    [Ishan Shrivastava] : added OrderMain

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/OrderMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.inheritance;
+
+public class OrderMain {
+    public static void main(String[] args) {
+        DeliveredOrder o = new DeliveredOrder();
+        System.out.println(o.getOrderStatus());
+    }
+}
