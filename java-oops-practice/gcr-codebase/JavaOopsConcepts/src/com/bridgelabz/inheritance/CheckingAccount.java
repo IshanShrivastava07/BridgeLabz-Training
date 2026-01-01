39d5ce717ffc9be0ec8d3759c60bd3adcb38aaf0
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-01 13:24:08 +0530

    [Ishan Shrivastava] : added CheckingAccount

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/CheckingAccount.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.inheritance;
+
+public class CheckingAccount extends BankAccount {
+    int withdrawalLimit;
+
+    void displayAccountType() {
+        System.out.println("Checking Account");
+    }
+}
