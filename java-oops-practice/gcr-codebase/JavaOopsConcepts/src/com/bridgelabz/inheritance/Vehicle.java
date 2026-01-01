73fb253801669887cf0988ccdc3ccb42c69542a1
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-01 13:29:13 +0530

    [Ishan Shrivastava] : added Vehicle

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/Vehicle.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.inheritance;
+
+public class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    void displayInfo() {
+        System.out.println(maxSpeed + " | " + fuelType);
+    }
+}
