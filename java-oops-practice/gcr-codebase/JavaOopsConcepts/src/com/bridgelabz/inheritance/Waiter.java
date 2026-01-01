253ff691f98d7ac06980b98e3e390a89f603d81d
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-01 13:29:28 +0530

    [Ishan Shrivastava] : added Waiter

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/Waiter.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.inheritance;
+
+public class Waiter extends Person implements Worker {
+    public void performDuties() {
+        System.out.println("Waiter serves food `1");
+    }
+}
