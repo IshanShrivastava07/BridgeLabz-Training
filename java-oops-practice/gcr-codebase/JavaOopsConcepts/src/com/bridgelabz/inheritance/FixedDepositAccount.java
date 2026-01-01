cf5a5af92613557f855e15921af0cdb8fed419c5
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-01 13:26:03 +0530

    [Ishan Shrivastava] : added FixedDepositAccount

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/FixedDepositAccount.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.inheritance;
+
+public class FixedDepositAccount extends BankAccount {
+    int tenure;
+
+    void displayAccountType() {
+        System.out.println("Fixed Deposit Account");
+    }
+}
