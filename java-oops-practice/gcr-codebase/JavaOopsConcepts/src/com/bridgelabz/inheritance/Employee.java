e6baca457cadd1871945e59d03bf366aad8d1895
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-01 13:25:16 +0530

    [Ishan Shrivastava] : added Employee

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/Employee.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.inheritance;
+
+public class Employee {
+    String name;
+    int id;
+    double salary;
+
+    void displayDetails() {
+        System.out.println(name + " | " + id + " | " + salary);
+    }
+}
