d51686ef35ff3af2bdc2cee80f5cbc18903cf21e
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-01 13:28:26 +0530

    [Ishan Shrivastava] : added SavingsAccount

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/inheritance/SavingsAccount.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.inheritance;
+
+public class SavingsAccount extends BankAccount {
+    double interestRate;
+
+    void displayAccountType() {
+        System.out.println("Savings Account");
+    }
+}
