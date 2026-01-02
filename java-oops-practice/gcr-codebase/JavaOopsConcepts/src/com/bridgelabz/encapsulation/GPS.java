7422b87a5c07a3645071f7a91e32c2ba58e5bfc4
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-02 12:33:49 +0530

    [Ishan Shrivastava] : added GPS

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/encapsulation/GPS.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.encapsulation;
+
+// GPS interface for ride vehicles
+public interface GPS {
+
+    String getCurrentLocation();
+
+    void updateLocation(String newLocation);
+}
