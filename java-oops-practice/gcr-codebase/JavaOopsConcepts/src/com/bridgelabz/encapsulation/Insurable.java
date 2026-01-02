5776ead88333cbd56db492755f90e78a70202f22
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-02 12:34:07 +0530

    [Ishan Shrivastava] : added Insurable

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/encapsulation/Insurable.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.encapsulation;
+
+// Interface for vehicles that have insurance
+public interface Insurable {
+
+    double calculateInsurance();
+
+    String getInsuranceDetails();
+}
+
