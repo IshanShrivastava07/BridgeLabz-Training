8f251d61a2af75f6791811e354c3d41162d91ad4
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-02 12:35:24 +0530

    [Ishan Shrivastava] : added Taxable

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/encapsulation/Taxable.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.encapsulation;
+// Interface for products that have tax
+public interface Taxable {
+
+    // Method to calculate tax amount
+    double calculateTax();
+
+    // Method to show tax information
+    String getTaxDetails();
+}
