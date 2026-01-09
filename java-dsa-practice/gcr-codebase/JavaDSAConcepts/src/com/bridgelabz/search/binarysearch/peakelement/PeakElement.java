52606996a5e1482dcea3160fd54482d833a151eb
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-09 14:38:15 +0530

    [Ishan Shrivastava] : added PeakElement

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/search/binarysearch/peakelement/PeakElement.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.search.binarysearch.peakelement;
+
+class PeakElement {
+    static int peak(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (arr[m] < arr[m + 1]) l = m + 1;
+            else r = m;
+        }
+        return l;
+    }
+}
