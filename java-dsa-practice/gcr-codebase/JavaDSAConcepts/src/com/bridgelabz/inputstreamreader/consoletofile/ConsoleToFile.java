65ccc13044c4c23e8b5036c37eb956654bff0a3c
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-09 13:22:50 +0530

    [Ishan Shrivastava] : added ConsoleToFile

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/inputstreamreader/consoletofile/ConsoleToFile.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.inputstreamreader.consoletofile;
+
+import java.io.*;
+
+class ConsoleToFile {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FileWriter fw = new FileWriter("output.txt");
+
+        String line;
+        while (!(line = br.readLine()).equalsIgnoreCase("exit")) {
+            fw.write(line + "\n");
+        }
+        fw.close();
+    }
+}
+
