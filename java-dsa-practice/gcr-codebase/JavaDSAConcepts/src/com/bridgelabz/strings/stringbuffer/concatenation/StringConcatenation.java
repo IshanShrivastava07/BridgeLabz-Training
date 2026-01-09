7e4e76ee7614756a0212074d7f7581f4c0bc2a9c
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-09 14:16:40 +0530

    [Ishan Shrivastava] : added Concatenation

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/strings/stringbuffer/concatenation/StringConcatenation.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.strings.stringbuffer.concatenation;
+
+class StringConcatenation {
+    public static void main(String[] args) {
+        String[] arr = {"Java", " ", "is", " ", "fast"};
+        StringBuffer sb = new StringBuffer();
+
+        for (String s : arr) sb.append(s);
+        System.out.println(sb.toString());
+    }
+}
