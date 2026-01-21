0ff875c39a1d8ee8b6737c3b3f8d9a02f62a32be
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-21 13:11:20 +0530

    [Ishan Shrivastava] : added fitnesstracker sorting scenario

---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBased/src/com/bridgelabz/sortingalgorithm/fitnesstracker/User.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.sortingalgorithm.fitnesstracker;
+
+public class User {
+    String name;
+    int steps;
+
+    public User(String name, int steps) {
+        this.name = name;
+        this.steps = steps;
+    }
+}
