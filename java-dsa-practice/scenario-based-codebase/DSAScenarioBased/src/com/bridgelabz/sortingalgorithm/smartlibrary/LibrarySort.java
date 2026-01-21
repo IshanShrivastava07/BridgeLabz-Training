df64a55917328ededbf57c26d910e0f37d9778dc
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-21 13:11:57 +0530

    [Ishan Shrivastava] : added smartlibrary sorting scenario

---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBased/src/com/bridgelabz/sortingalgorithm/smartlibrary/LibrarySort.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.sortingalgorithm.smartlibrary;
+
+public class LibrarySort {
+
+    // Insertion Sort by title (A–Z)
+    public static void insertionSort(Book[] books, int size) {
+        for (int i = 1; i < size; i++) {
+            Book key = books[i];
+            int j = i - 1;
+
+            while (j >= 0 && books[j].title.compareToIgnoreCase(key.title) > 0) {
+                books[j + 1] = books[j];
+                j--;
+            }
+
+            books[j + 1] = key;
+        }
+    }
+
+    public static void display(Book[] books, int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.println(books[i].title + " by " + books[i].author);
+        }
+    }
+}
+
