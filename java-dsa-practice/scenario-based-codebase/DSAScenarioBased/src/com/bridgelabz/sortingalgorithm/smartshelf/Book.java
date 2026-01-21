642266ca00cd80e52f9a1c4dbbeea83e7f9a7520
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-21 13:12:00 +0530

    [Ishan Shrivastava] : added smartshelf sorting scenario

---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBased/src/com/bridgelabz/sortingalgorithm/smartshelf/Book.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.sortingalgorithm.smartshelf;
+
+public class Book {
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
