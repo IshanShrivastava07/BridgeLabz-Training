877fa01dff5d8f4eacccb2a96ba68563c9bf1c40
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-21 15:22:21 +0530

    Updated Repository Structure

--- java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/bridgelabz/stackqueuehashmap/longestconsecutivesequence/LongestSequenceMain.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.stackqueuehashmap.longestconsecutivesequence;
-
-public class LongestSequenceMain {
-    public static void main(String[] args) {
-        int[] nums = {100, 4, 200, 1, 3, 2};
-
-        System.out.println(LongestSequence.longestConsecutive(nums)); 
-    }
-}
