9638e76eb6972924d9c480c36a9073800682426a
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-21 13:11:45 +0530

    [Ishan Shrivastava] : added fleetmanager sorting scenario

---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBased/src/com/bridgelabz/sortingalgorithm/fleetmanager/Vehicle.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.sortingalgorithm.fleetmanager;
+
+public class Vehicle {
+    String vehicleId;
+    int mileage;
+
+    public Vehicle(String vehicleId, int mileage) {
+        this.vehicleId = vehicleId;
+        this.mileage = mileage;
+    }
+}
+
