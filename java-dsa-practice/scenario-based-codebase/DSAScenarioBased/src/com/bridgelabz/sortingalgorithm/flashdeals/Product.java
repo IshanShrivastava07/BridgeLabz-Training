df850567be858e72190badd355d38cdf08671803
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-21 13:11:26 +0530

    [Ishan Shrivastava] : added flashdeals sorting scenario

---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBased/src/com/bridgelabz/sortingalgorithm/flashdeals/Product.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.sortingalgorithm.flashdeals;
+
+public class Product {
+String productId;
+String name;
+double discount;
+
+public Product(String productId, String name, double discount) {
+    this.productId = productId;
+    this.name = name;
+    this.discount = discount;
+}
+}
