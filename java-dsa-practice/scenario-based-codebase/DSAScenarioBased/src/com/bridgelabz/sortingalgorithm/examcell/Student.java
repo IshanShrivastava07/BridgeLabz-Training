753a31752b702a56f2bfcecc91c715704f48b95b
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-21 13:11:15 +0530

    [Ishan Shrivastava] : added examcell sorting scenario

---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBased/src/com/bridgelabz/sortingalgorithm/examcell/Student.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.sortingalgorithm.examcell;
+
+public class Student {
+ int rollNo;
+ String name;
+ double marks;
+ 
+ public Student(double marks, String name, int rollno) {
+	 this.marks = marks;
+	 this.name = name;
+	 this.rollNo = rollNo;
+}
+}
