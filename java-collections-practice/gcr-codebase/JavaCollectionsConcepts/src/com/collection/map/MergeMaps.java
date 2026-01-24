9c3436201bc65618bd3f3c0238443830244b31e9
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-24 13:25:05 +0530

    [Ishan Shrivastava] : added Merge Maps in Map Interface

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/collection/map/MergeMaps.java
@@ -0,0 +1,24 @@
+package com.collection.map;
+
+import java.util.*;
+
+public class MergeMaps {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> m1 = new HashMap<>();
+        m1.put("A", 1);
+        m1.put("B", 2);
+
+        Map<String, Integer> m2 = new HashMap<>();
+        m2.put("B", 3);
+        m2.put("C", 4);
+
+        for (String key : m2.keySet()) {
+            m1.put(key, m1.getOrDefault(key, 0) + m2.get(key));
+        }
+
+        System.out.println(m1);   // {A=1, B=5, C=4}
+    }
+}
+
