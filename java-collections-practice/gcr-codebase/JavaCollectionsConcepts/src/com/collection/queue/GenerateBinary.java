b7919dc7df86fd8e851b3b62f4aa0e9a9941b6b4
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-24 13:20:56 +0530

    [Ishan Shrivastava] : added Generate Binary in Queue Interface

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/collection/queue/GenerateBinary.java
@@ -0,0 +1,21 @@
+package com.collection.queue;
+
+import java.util.*;
+
+public class GenerateBinary {
+
+    public static void main(String[] args) {
+        int n = 5;
+        Queue<String> q = new LinkedList<>();
+
+        q.add("1");
+
+        for (int i = 0; i < n; i++) {
+            String s = q.remove();
+            System.out.print(s + " ");
+
+            q.add(s + "0");
+            q.add(s + "1");
+        }
+    }
+}
