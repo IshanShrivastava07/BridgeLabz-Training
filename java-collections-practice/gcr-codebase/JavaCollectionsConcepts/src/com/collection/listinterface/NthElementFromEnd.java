7df7e2264436e17bdbc80184b9c5ca5d28047588
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-24 13:08:11 +0530

    [Ishan Shrivastava] : added Nth Element from End in List Interface

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/collection/listinterface/NthElementFromEnd.java
@@ -0,0 +1,32 @@
+package com.collection.listInterface;
+
+import java.util.*;
+
+
+public class NthElementFromEnd {
+public static void main(String[] args) {
+	LinkedList<String> list = new LinkedList<>(Arrays.asList("A","B","C","D","E"));
+	int n = 2;
+	String res = nthFromEnd(list,n);
+	System.out.println(list);
+}
+	
+public static String nthFromEnd(LinkedList<String> list, int n) {
+	Iterator<String> fast = list.iterator();
+	Iterator<String> slow = list.iterator();
+	
+	for(int i = 0; i < n; i++) {
+		
+		if(!fast.hasNext()) return null;
+		fast.next();
+	}
+	
+	while(fast.hasNext()) {
+		fast.next();
+		slow.next();
+	}
+	;
+	return slow.next();
+}
+	
+}
