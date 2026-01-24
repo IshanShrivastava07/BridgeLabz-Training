dfd68d747c77955c3f0b9c781b6a0572d77d8d5f
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-24 13:09:19 +0530

    [Ishan Shrivastava] : added Reverse ArrayList in List Interface

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/collection/listinterface/ReverseArraylist.java
@@ -0,0 +1,21 @@
+package com.collection.listInterface; 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReverseArraylist {
+public static void main(String[] args) {
+	ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+	
+	int left = 0;
+	int right = list.size() - 1;
+	
+	while(left < right) {
+		int temp = list.get(left);
+		list.set(left,list.get(right));
+		list.set(right,temp);
+		left++;
+		right--;
+	}
+	System.out.println(list);
+}
+}
