68a2e6c3b41c9b17eb6b4f1c2da242ec7b2ad11b
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-27 12:14:06 +0530

    [Ishan Shrivastava] : added InsufficientBalanceException in Bank Transaction System

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/exceptionhandling/banktransactionsystem/InsufficientBalanceException.java
@@ -0,0 +1,9 @@
+package com.exceptionhandling.banktransactionsystem;
+
+class InsufficientBalanceException extends Exception {
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
