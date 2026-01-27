1783bd252d195f15ef871da560764e0901d3d1f9
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-27 12:18:13 +0530

    [Ishan Shrivastava] : added CapatilizedWords in Extraction Uppercase Wordsl Extraction

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/regex/extraction/UppercaseWordslExtraction/CapatilizedWords.java
@@ -0,0 +1,16 @@
+package com.regex.extraction.UppercaseWordslExtraction;
+
+import java.util.regex.*;
+
+public class CapatilizedWords {
+    public static void main(String[] args) {
+        String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+
+        Pattern p = Pattern.compile("\\b[A-Z][a-z]*\\b");
+        Matcher m = p.matcher(text);
+
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
+}
