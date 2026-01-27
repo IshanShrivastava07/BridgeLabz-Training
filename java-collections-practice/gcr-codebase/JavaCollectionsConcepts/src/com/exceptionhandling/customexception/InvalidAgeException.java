fe25178acd654d80ffb893283456643e78a2605b
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-27 12:14:12 +0530

    [Ishan Shrivastava] : added InvalidAgeException in Custom Exception

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/exceptionhandling/customexception/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package com.exceptionhandling.customexception;
+
+class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
