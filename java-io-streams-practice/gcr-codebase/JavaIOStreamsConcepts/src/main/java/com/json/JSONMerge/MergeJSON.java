113f2a3429ea547bd8deb16703ebdec5f9b13d9e
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-30 17:08:19 +0530

    [Ishan Shrivastava] : added JSON Merge in Json

---
+++ java-io-streams-practice/gcr-codebase/JavaIOStreamsConcepts/src/main/java/com/json/JSONMerge/MergeJSON.java
@@ -0,0 +1,21 @@
+package com.json.JSONMerge;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class MergeJSON {
+    public static void main(String[] args) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        ObjectNode json1 = mapper.createObjectNode();
+        json1.put("name", "Ishan");
+
+        ObjectNode json2 = mapper.createObjectNode();
+        json2.put("age", 22);
+
+        json1.setAll(json2);
+
+        System.out.println(json1.toPrettyString());
+    }
+}
