4d0d09b40617bc417baa63340f5784431ef7e327
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-01-31 11:38:50 +0530

    [Ishan Shrivastava] : added Merge JSON in Handson JSON

---
+++ java-io-streams-practice/gcr-codebase/JavaIOStreamsConcepts/src/main/java/com/HandsonJSON/MergeJSON/MergeJson.java
@@ -0,0 +1,17 @@
+package com.HandsonJSON.MergeJSON;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.File;
+
+public class MergeJson {
+    public static void main(String[] args) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode json1 = (ObjectNode) mapper.readTree(new File("file1.json"));
+        ObjectNode json2 = (ObjectNode) mapper.readTree(new File("file2.json"));
+
+        json1.setAll(json2);
+        System.out.println(json1.toPrettyString());
+    }
+}
