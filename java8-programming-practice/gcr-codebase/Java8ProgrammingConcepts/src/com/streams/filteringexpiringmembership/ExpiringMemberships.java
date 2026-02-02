eb96d3c0d8bf6f80ec73c8dae1fe886a0096c71e
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-02-02 15:35:18 +0530

    [Ishan Shrivastava] : added ExpiringMemberships in Filtering Expiring Membership

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/streams/filteringexpiringmembership/ExpiringMemberships.java
@@ -0,0 +1,32 @@
+package com.streams.filteringexpiringmembership;
+
+import java.time.*;
+import java.util.*;
+
+class Member {
+    String name;
+    LocalDate expiry;
+
+    Member(String name, LocalDate expiry) {
+        this.name = name;
+        this.expiry = expiry;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
+
+public class ExpiringMemberships {
+    public static void main(String[] args) {
+
+        List<Member> members = List.of(
+                new Member("A", LocalDate.now().plusDays(10)),
+                new Member("B", LocalDate.now().plusDays(40))
+        );
+
+        members.stream()
+               .filter(m -> m.expiry.isBefore(LocalDate.now().plusDays(30)))
+               .forEach(System.out::println);
+    }
+}
