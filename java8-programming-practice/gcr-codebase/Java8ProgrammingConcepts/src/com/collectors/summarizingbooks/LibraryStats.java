fe05a0b650dcf7930f341201a4cb57670b5f1d4e
Ishan Shrivastava <168469318+IshanShrivastava07@users.noreply.github.com>
2026-02-02 15:47:56 +0530

    [Ishan Shrivastava] : added LibraryStats in Summarizing Books

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingConcepts/src/com/collectors/summarizingbooks/LibraryStats.java
@@ -0,0 +1,48 @@
+package com.collectors.summarizingbooks;
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.IntSummaryStatistics;
+
+class Book {
+    String title;
+    String genre;
+    int pages;
+
+    Book(String title, String genre, int pages) {
+        this.title = title;
+        this.genre = genre;
+        this.pages = pages;
+    }
+
+    String getGenre() { return genre; }
+    int getPages() { return pages; }
+}
+
+public class LibraryStats {
+    public static void main(String[] args) {
+
+        List<Book> books = List.of(
+            new Book("Book A", "Fiction", 320),
+            new Book("Book B", "Fiction", 280),
+            new Book("Book C", "Science", 450),
+            new Book("Book D", "Science", 380),
+            new Book("Book E", "History", 500)
+        );
+
+        Map<String, IntSummaryStatistics> statsByGenre =
+            books.stream()
+                 .collect(Collectors.groupingBy(
+                     Book::getGenre,
+                     Collectors.summarizingInt(Book::getPages)
+                 ));
+
+        statsByGenre.forEach((genre, stats) -> {
+            System.out.println("Genre: " + genre);
+            System.out.println("Total Pages: " + stats.getSum());
+            System.out.println("Average Pages: " + stats.getAverage());
+            System.out.println("Max Pages: " + stats.getMax());
+            System.out.println();
+        });
+    }
+}
